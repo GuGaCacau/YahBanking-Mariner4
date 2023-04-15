6dfefc972b92f1cc87e9435d9271d71c911771fc
GuGaCacau <gugacacau2@gmail.com>
2023-04-15 20:05:02 -0300

    Requests Customizados

---
+++ app/Http/Requests/ClientRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientRequest extends FormRequest
+{
+    
+    public function rules(): array
+    {
+        //Validando o tamanho e tipo da imagem do avatar e se existem as outras infos
+        return [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+            'avatar' => 'required|image|mimes:png,jpg,jpeg,gif,svg',
+        ];
+    }
+
+    public function messages()
+    {
+        //Mensagens de erro para cada caso
+        return [
+            'first_name.required' => 'Por favor, complete o campo de "Nome".',
+            'last_name.required' => 'Por favor, complete o campo de "Sobrenome".',
+            'email.required' => 'Por favor, complete o campo de "Email".',
+            'avatar.required' => 'Por favor, escolha um avatar.',
+            'avatar.image' => 'Por favor, escolha um avatar válido (imagem).',
+            'avatar.mimes' => 'Os formatos de imagem permitidos são png, jpg, jpeg, gif e svg.',
+        ];
+    }
+}
