309d50d60d749288c65ceb3b59ce40ecf4952cee
GuGaCacau <gugacacau2@gmail.com>
2023-04-18 15:06:14 -0300

    Revert SoftDeletes

--- database/migrations/2023_04_14_202121_create_client_investment_table.php
+++ database/migrations/2023_04_14_202121_create_client_investment_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->float('investment_amount', 8, 2);
 
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
